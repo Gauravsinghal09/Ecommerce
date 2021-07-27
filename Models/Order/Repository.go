1cddcba8af60d1876abe44b8835b5054cb6ea1a9
Gaurav Singhal <gsinghal@me.iitr.ac.in>
2021-07-27 12:38:08 +0530

    adds Repository code for database operation through customer and order

---
+++ Models/Order/Repository.go
@@ -0,0 +1,33 @@
+package Order
+
+import (
+	"github.com/Gauravsinghal09/Ecommerce/Config"
+	"gorm.io/gorm"
+)
+
+func GetOrders(order *[]Orders) (err error) {
+	if err = Config.DB.Find(order).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func CreateOrder(order *Orders) (err error) {
+	err = Config.DB.Transaction(func(tx *gorm.DB) error {
+		if err = Config.DB.Create(order).Error; err != nil {
+			return err
+		}
+		if err = Config.DB.Model(order).Update("Status", "Order placed").Error; err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func GetOrderById(order *Orders, OrderId string) (err error) {
+	if err = Config.DB.Where("id = ?", OrderId).First(order).Error; err != nil {
+		return err
+	}
+	return nil
+}
